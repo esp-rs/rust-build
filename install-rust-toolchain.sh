038cc6a3cf1f5d1d72280785b708e6fa6c0beecf
Juraj Michalek <juraj.michalek@espressif.com>
2021-09-16 06:00:17 +0200

    tools: fix download for macOS x86_64

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -9,10 +9,14 @@ ARCH=`rustup show | grep "Default host" | sed -e 's/.* //'`
 #ARCH="x86_64-unknown-linux-gnu"
 #ARCH="x86_64-pc-windows-msvc"
 
+LLVM_RELEASE="esp-12.0.1-20210823"
+
 if [ ${ARCH} == "aarch64-apple-darwin" ]; then
     LLVM_ARCH="aarch64-apple-darwin"
 elif [ ${ARCH} == "x86_64-apple-darwin" ]; then
-    LLVM_ARCH="x86_64-apple-darwin"
+    #LLVM_ARCH="x86_64-apple-darwin"
+    LLVM_ARCH="macos"
+    LLVM_RELEASE="esp-12.0.1-20210914"
 elif [ ${ARCH} == "x86_64-unknown-linux-gnu" ]; then
     LLVM_ARCH="linux-amd64"
 elif [ ${ARCH} == "x86_64-pc-windows-msvc" ]; then
@@ -27,10 +31,10 @@ VERSION="1.55.0-dev"
 RUST_DIST="rust-${VERSION}-${ARCH}"
 RUST_SRC_DIST="rust-src-${VERSION}"
 TOOLCHAIN_DESTINATION_DIR="$HOME/.rustup/toolchains/esp"
-LLVM_RELEASE="esp-12.0.1-20210823"
 LLVM_FILE="xtensa-esp32-elf-llvm12_0_1-${LLVM_RELEASE}-${LLVM_ARCH}.tar.xz"
 IDF_TOOLS_PATH="$HOME/.espressif"
 IDF_TOOL_XTENSA_ELF_CLANG="${IDF_TOOLS_PATH}/tools/xtensa-esp32-elf-clang/${LLVM_RELEASE}-${ARCH}"
+RUST_DIST_URL="https://github.com/esp-rs/rust-build/releases/download/v${VERSION}/${RUST_DIST}.tar.xz"
 
 if [ -d ${TOOLCHAIN_DESTINATION_DIR} ]; then
     echo "Previous installation of toolchain exist in: ${TOOLCHAIN_DESTINATION_DIR}"
@@ -42,7 +46,8 @@ fi
 if [ ! -d ${TOOLCHAIN_DESTINATION_DIR} ]; then
     mkdir -p ${TOOLCHAIN_DESTINATION_DIR}
     if [ ! -f ${RUST_DIST}.tar.xz ]; then
-        curl -LO "https://github.com/esp-rs/rust-build/releases/download/v${VERSION}/${RUST_DIST}.tar.xz"
+        echo "** downloading: ${RUST_DIST_URL}"
+        curl -LO "${RUST_DIST_URL}"
         tar xf ${RUST_DIST}.tar.xz
     fi
     ./${RUST_DIST}/install.sh --destdir=${TOOLCHAIN_DESTINATION_DIR} --prefix="" --without=rust-docs
@@ -59,6 +64,7 @@ rustup default esp
 echo -n "* installing ${IDF_TOOL_XTENSA_ELF_CLANG} - "
 if [ ! -d ${IDF_TOOL_XTENSA_ELF_CLANG} ]; then
     curl -LO "https://github.com/espressif/llvm-project/releases/download/${LLVM_RELEASE}/${LLVM_FILE}"
+    mkdir -p `dirname "${IDF_TOOL_XTENSA_ELF_CLANG}"`
     tar xf ${LLVM_FILE} -C "${IDF_TOOL_XTENSA_ELF_CLANG}"
     echo "done"
 else
