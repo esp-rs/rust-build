ab44c3a0e847b85dcecfe92db1ef9faea3eaea0d
Juraj Michalek <juraj.michalek@espressif.com>
2021-10-25 11:48:29 +0200

    test: add script for testing installation of toolchain

--- test-rust-toolchain.sh
+++ test-rust-toolchain.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
-./install-rust-toolchain.sh --export-file export-rust.sh
-. ./export-rust.sh
+# Default values
+TOOLCHAIN_VERSION="1.56.0-dev"
+TOOLCHAIN_PREFIX="esp"
+BUILD_TARGET="xtensa-esp32-espidf"
 
-RUST_DEMO="rust-esp32-std-demo"
+# Process positional arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -t|--toolchain-version)
+      TOOLCHAIN_VERSION="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n|--toolchain-prefix)
+      TOOLCHAIN_PREFIX="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -b|--target)
+      BUILD_TARGET="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "Processing configuration:"
+echo "--target             = ${BUILD_TARGET}"
+echo "--toolchain-prefix   = ${TOOLCHAIN_PREFIX}"
+echo "--toolchain-version  = ${TOOLCHAIN_VERSION}"
+
+TOOLCHAIN_NAME="${TOOLCHAIN_PREFIX}-${TOOLCHAIN_VERSION}"
+EXPORT_FILE="export-rust-${TOOLCHAIN_NAME}.sh"
+
+./install-rust-toolchain.sh --installation-mode reinstall --export-file "${EXPORT_FILE}" --toolchain-version ${TOOLCHAIN_VERSION}
+. "./${EXPORT_FILE}"
+
+RUST_STD_DEMO="rust-esp32-std-demo"
 
 if [ ! -d "" ]; then
-    git clone https://github.com/ivmarkov/${RUST_DEMO}.git
+    git clone https://github.com/ivmarkov/${RUST_STD_DEMO}.git
 fi
 
-cd "${RUST_DEMO}"
-cargo +esp build --target xtensa-esp32-espidf
+cd "${RUST_STD_DEMO}"
+export RUST_ESP32_STD_DEMO_WIFI_SSID="rust"
+export RUST_ESP32_STD_DEMO_WIFI_PASS="for-esp32"
+cargo +${TOOLCHAIN_NAME} build --target ${BUILD_TARGET}
