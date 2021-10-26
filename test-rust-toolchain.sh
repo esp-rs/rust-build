757e7445ff7f7766021d7e38ad48b42dd0f9bbbc
Juraj Michálek <juraj.michalek@espressif.com>
2021-10-26 13:39:03 +0200

    test: all build targets on macOS self-hosted

--- test-rust-toolchain.sh
+++ test-rust-toolchain.sh
@@ -7,6 +7,7 @@ if [ -z "${RUSTUP_HOME}" ]; then
 fi
 TOOLCHAIN_PREFIX="esp"
 BUILD_TARGET="xtensa-esp32-espidf"
+INSTALLATION_MODE="install" # reinstall, uninstall
 
 # Process positional arguments
 POSITIONAL=()
@@ -29,6 +30,11 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    -i|--installation-mode)
+      INSTALLATION_MODE="$2"
+      shift # past argument
+      shift # past value
+      ;;
     *)    # unknown option
       POSITIONAL+=("$1") # save it in an array for later
       shift # past argument
@@ -39,6 +45,7 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "Processing configuration:"
+echo "--installation-mode  = ${INSTALLATION_MODE}"
 echo "--target             = ${BUILD_TARGET}"
 echo "--toolchain-prefix   = ${TOOLCHAIN_PREFIX}"
 echo "--toolchain-version  = ${TOOLCHAIN_VERSION}"
@@ -46,7 +53,7 @@ echo "--toolchain-version  = ${TOOLCHAIN_VERSION}"
 TOOLCHAIN_NAME="${TOOLCHAIN_PREFIX}-${TOOLCHAIN_VERSION}"
 EXPORT_FILE="export-rust-${TOOLCHAIN_NAME}.sh"
 
-./install-rust-toolchain.sh --installation-mode reinstall \
+./install-rust-toolchain.sh --installation-mode ${INSTALLATION_MODE} \
     --export-file "${EXPORT_FILE}" \
     --toolchain-destination "${RUSTUP_HOME}/toolchains/${TOOLCHAIN_NAME}" \
     --toolchain-version ${TOOLCHAIN_VERSION}
@@ -54,11 +61,18 @@ EXPORT_FILE="export-rust-${TOOLCHAIN_NAME}.sh"
 
 RUST_STD_DEMO="rust-esp32-std-demo"
 
-if [ ! -d "" ]; then
+if [ ! -d "${RUST_STD_DEMO}" ]; then
     git clone https://github.com/ivmarkov/${RUST_STD_DEMO}.git
 fi
 
 cd "${RUST_STD_DEMO}"
 export RUST_ESP32_STD_DEMO_WIFI_SSID="rust"
 export RUST_ESP32_STD_DEMO_WIFI_PASS="for-esp32"
-cargo +${TOOLCHAIN_NAME} build --target ${BUILD_TARGET}
+
+if [ "${BUILD_TARGET}" == "all" ]; then
+  cargo +${TOOLCHAIN_NAME} build --target xtensa-esp32-espidf --installation-mode reinstall
+  cargo +${TOOLCHAIN_NAME} build --target xtensa-esp32s2-espidf
+  cargo +${TOOLCHAIN_NAME} build --target riscv32imc-esp-espidf
+else
+  cargo +${TOOLCHAIN_NAME} build --target ${BUILD_TARGET}
+fi
