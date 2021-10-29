8d6de4feedf14f0dc2f78fcf6d4f94da26404499
Nicholas Latham <nicholas-l@users.noreply.github.com>
2021-10-29 16:31:39 +0100

    Use OR in install rather than pipe

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -92,7 +92,7 @@ command -v rustup || install_rust
 command -v cargo || source_cargo
 rustup toolchain list | grep stable || install_rust_toolchain stable
 rustup toolchain list | grep nightly || install_rust_toolchain nightly
-rustfmt --version 2> /dev/null | install_rustfmt
+rustfmt --version 2> /dev/null || install_rustfmt
 
 ARCH=`rustup show | grep "Default host" | sed -e 's/.* //'`
 #ARCH="aarch64-apple-darwin"
