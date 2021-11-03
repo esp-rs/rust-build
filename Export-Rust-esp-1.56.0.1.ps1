0ce370686950b7e9ab918bf00af2d250d4e55d63
Juraj Michalek <juraj.michalek@gmail.com>
2021-11-03 15:39:28 +0100

    test: remove export file

--- Export-Rust-esp-1.56.0.1.ps1
+++
@@ -1,3 +0,0 @@
-
-$env:PATH+=";C:\Users\JurajMichálek/.espressif/tools/xtensa-esp32-elf-clang/esp-12.0.1-20210914-x86_64-pc-windows-msvc/bin/"
-$env:LIBCLANG_PATH="C:\Users\JurajMichálek/.espressif/tools/xtensa-esp32-elf-clang/esp-12.0.1-20210914-x86_64-pc-windows-msvc/bin/libclang.dll"
