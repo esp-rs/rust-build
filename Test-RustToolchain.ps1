cea188ce03a0b76eeb82e179b9c7fb707c213e6e
Juraj Michálek <juraj.michalek@espressif.com>
2021-11-03 12:25:46 +0100

    tests: Windows test using espflash

--- Test-RustToolchain.ps1
+++ Test-RustToolchain.ps1
@@ -4,27 +4,38 @@ param (
     [String]
     $ToolchainVersion = '1.56.0.1',
     [String]
-    $BuildTarget = "xtensa-esp32-espidf",
+    [ValidateSet("xtensa-esp32-espidf", "xtensa-esp32s2-espidf", "xtensa-esp32s3-espidf", "riscv32imc-esp-espidf")]
+    $Target = "xtensa-esp32-espidf",
     [String]
-    $InstallationMode = 'reinstall' # install, reinstall, uninstall
+    [ValidateSet("install", "reinstall", "uninstall", "skip")]
+    $InstallationMode = 'reinstall',
+    [String]
+    [ValidateSet("build", "flash", "monitor")]
+    $TestMode = "build",
+    [String]
+    $TestPort = "COM5"
 )
 
 $ErrorActionPreference = "Stop"
 $RustStdDemo = "rust-esp32-std-demo"
 
 "Processing configuration:"
-"-BuildTarget      = ${BuildTarget}"
+"-Target           = ${Target}"
 "-ToolchainVersion = ${ToolchainVersion}"
+"-TestMode         = ${TestMode}"
+"-TestPort         = ${TestPort}"
 
 $ToolchainPrefix = "esp"
 $ToolchainName = "${ToolchainPrefix}-${ToolchainVersion}"
 $ExportFile="Export-Rust-${ToolchainName}.ps1"
 
-./Install-RustToolchain.ps1 `
-    -ExportFile ${ExportFile} `
-    -InstallationMode ${InstallationMode} `
-    -ToolchainVersion ${ToolchainVersion} `
-    -ToolchainDestination "${HOME}/.rustup/toolchains/${ToolchainName}"
+if ("skip" -ne $InstallationMode) {
+    ./Install-RustToolchain.ps1 `
+        -ExportFile ${ExportFile} `
+        -InstallationMode ${InstallationMode} `
+        -ToolchainVersion ${ToolchainVersion} `
+        -ToolchainDestination "${HOME}/.rustup/toolchains/${ToolchainName}"
+}
 
 . ./${ExportFile}
 
@@ -36,5 +47,12 @@ Push-Location ${RustStdDemo}
 $env:RUST_ESP32_STD_DEMO_WIFI_SSID="rust"
 $env:RUST_ESP32_STD_DEMO_WIFI_PASS="for-esp32"
 
-cargo +${ToolchainName} build --target ${BuildTarget}
+if ("build" -eq $TestMode) {
+    cargo +${ToolchainName} build --target ${Target}
+} elseif ("flash" -eq $TestMode) {
+    "cargo +${ToolchainName} espflash --target ${Target} $TestPort"
+    cargo +${ToolchainName} espflash --target ${Target} $TestPort
+} elseif ("monitor" -eq $TestMode) {
+    cargo +${ToolchainName} espflash --monitor --target ${Target} $TestPort
+}
 Pop-Location
