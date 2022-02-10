8dfc0f4632f186807fcc5aad72422ba5d9f89190
Juraj Michálek <juraj.michalek@espressif.com>
2022-02-10 17:18:40 +0100

    ci: switch to old idf.py-first approach

--- support/esp-rs-rust/entrypoint.sh
+++ support/esp-rs-rust/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-#. $IDF_PATH/export.sh
+. $IDF_PATH/export.sh
+. /opt/rust-export.sh
+export LIBCLANG_PATH=/opt/.espressif/tools/xtensa-esp32-elf-clang/esp-13.0.0-20211203-aarch64-unknown-linux-gnu/xtensa-esp32-elf-clang/lib/
 cat /etc/motd
 
 exec "$@"
