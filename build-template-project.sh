2de582ac4d11ba824027ee932d039983f97cd49d
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-07-19 16:20:09 +0200

    Add buil-template-project script

---
+++ build-template-project.sh
@@ -0,0 +1,10 @@
+set -ef
+
+source /home/esp/export-esp.sh
+if [[ "$1" == 'esp-idf-template' ]]; then
+    cargo generate --git https://github.com/esp-rs/esp-idf-template --branch fix/remove-toolchain-question cargo --name test-$2 --vcs none --silent -d mcu=$2 -d std=true -d espidfver=$3 -d devcontainer=false
+elif [[ "$1" == 'esp-template' ]]; then
+    cargo generate --git https://github.com/esp-rs/esp-template --name test-$2 --vcs none --silent -d mcu=$2 -d devcontainer=false
+fi
+cd test-$2
+cargo build
