4c12204e159e1b6ffa1d2751794baa4794728f0a
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-07-20 09:51:56 +0200

    Update esp-idf-template branch

--- build-template-project.sh
+++ build-template-project.sh
@@ -3,7 +3,7 @@ set -ef
 export USER=esp
 source /home/esp/export-esp.sh
 if [[ "$1" == 'esp-idf-template' ]]; then
-    cargo generate --git https://github.com/esp-rs/esp-idf-template --branch fix/remove-toolchain-question cargo --name test-$2 --vcs none --silent -d mcu=$2 -d std=true -d espidfver=$3 -d devcontainer=false
+    cargo generate --git https://github.com/esp-rs/esp-idf-template cargo --name test-$2 --vcs none --silent -d mcu=$2 -d std=true -d espidfver=$3 -d devcontainer=false
 elif [[ "$1" == 'esp-template' ]]; then
     cargo generate --git https://github.com/esp-rs/esp-template --name test-$2 --vcs none --silent -d mcu=$2 -d devcontainer=false
 fi
