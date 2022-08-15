0e787fe23eef3bb1b9cb34a7485629692ec2843f
Juraj Michálek <juraj.michalek@espressif.com>
2022-08-15 13:44:25 +0200

    ci: msys build using shallow repo

--- support/rust-build/x86_64-pc-windows-gnu/build-rust-toolchain-msys2.sh
+++ support/rust-build/x86_64-pc-windows-gnu/build-rust-toolchain-msys2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd c:
-git clone --recursive https://github.com/esp-rs/rust.git r
+git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git r
 cd r
 
 python3 src/bootstrap/configure.py --experimental-targets=Xtensa --enable-extended --tools=clippy,cargo,rustfmt --dist-compression-formats='xz' --host 'x86_64-pc-windows-gnu'
