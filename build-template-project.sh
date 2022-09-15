8b32a78a34469644b558dc5d3524bc376d0b8525
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-09-15 10:07:58 +0200

    ci: 🐛 Add `alloc` define to esp-template projects

--- build-template-project.sh
+++ build-template-project.sh
@@ -5,7 +5,7 @@ source /home/esp/export-esp.sh
 if [[ "$1" == 'esp-idf-template' ]]; then
     cargo generate --git https://github.com/esp-rs/esp-idf-template cargo --name test-$2 --vcs none --silent -d mcu=$2 -d std=true -d espidfver=$3 -d devcontainer=false
 elif [[ "$1" == 'esp-template' ]]; then
-    cargo generate --git https://github.com/esp-rs/esp-template --name test-$2 --vcs none --silent -d mcu=$2 -d devcontainer=false
+    cargo generate --git https://github.com/esp-rs/esp-template --name test-$2 --vcs none --silent -d mcu=$2 -d devcontainer=false -d alloc=false
 fi
 cd test-$2
 cargo build
