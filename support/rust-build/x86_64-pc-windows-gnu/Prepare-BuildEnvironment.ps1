1da3a0c664debb7dbbe419354987650c2301ce49
Juraj Michálek <juraj.michalek@espressif.com>
2022-11-01 14:58:47 +0100

    ci: windows self-hosted runner requires git, before launching msys2 commands

--- support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
+++ support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
@@ -1,6 +1,7 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 #choco install msys2 -y
 
-winget  install --id MSYS2.MSYS2
+winget install --id Git.Git
+winget install --id MSYS2.MSYS2
 
 c:\msys64\msys2_shell.cmd -mingw64
