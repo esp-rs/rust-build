3e3932731d196256d0cb377ff093e40c7fe5fa00
Juraj Michálek <juraj.michalek@espressif.com>
2023-01-26 17:36:35 +0100

    WIP: Update to 1.67.0.0 (#188)
    * ci: bump up version to 1.67.0.0
    * ci: enable lld
    * ci: add option to specify upload tag
    * ci: change default build value to specific tag
    * ci: use tag for upload to support draft releases
    * ci: missing enable-cargo-native-static in x86_64-apple-darwin
    * ci: add clippy, rustfmt and cargo to Windows package

--- support/rust-build/Repackage-RustRelease.ps1
+++ support/rust-build/Repackage-RustRelease.ps1
@@ -11,7 +11,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 $RustVersion="nightly"
-$ReleaseVersion="1.66.0.0"
+$ReleaseVersion="1.67.0.0"
 
 if (Test-Path -Path esp -PathType Container) {
     Remove-Item -Recurse -Force -Path esp
@@ -27,10 +27,30 @@ cp -Recurse .\rustc\lib ..\esp\
 cp -Recurse .\rustc\share ..\esp\
 cp -ErrorAction SilentlyContinue -Recurse .\rust-std-${DefaultHost}\lib\* ..\esp\lib\
 popd
+
 7z e rust-src-${RustVersion}.tar.xz
 7z x rust-src-${RustVersion}.tar
 pushd rust-src-${RustVersion}
 cp -ErrorAction SilentlyContinue -Recurse .\rust-src\lib\* ..\esp\lib\
+popd
+
+7z e cargo-${RustVersion}.tar.xz
+7z x cargo-${RustVersion}.tar
+pushd cargo-${RustVersion}
+cp -ErrorAction SilentlyContinue -Recurse .\cargo\bin\* ..\esp\bin\
+cp -ErrorAction SilentlyContinue -Recurse .\cargo\libexec ..\esp\
+popd
+
+7z e clippy-${RustVersion}.tar.xz
+7z x clippy-${RustVersion}.tar
+pushd clippy-${RustVersion}
+cp -ErrorAction SilentlyContinue -Recurse .\clippy-preview\bin\* ..\esp\bin\
+popd
+
+7z e rustfmt-${RustVersion}.tar.xz
+7z x rustfmt-${RustVersion}.tar
+pushd rustfmt-${RustVersion}
+cp -ErrorAction SilentlyContinue -Recurse .\rustfmt-preview\bin\* ..\esp\bin\
 popd
 
 # Clean up debug files
--- support/rust-build/x86_64-pc-windows-gnu/build-rust-toolchain-msys2.sh
+++ support/rust-build/x86_64-pc-windows-gnu/build-rust-toolchain-msys2.sh
@@ -4,6 +4,6 @@ cd c:
 git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git r
 cd r
 
-python3 src/bootstrap/configure.py --experimental-targets=Xtensa --enable-extended --tools=clippy,cargo,rustfmt --dist-compression-formats='xz' --host 'x86_64-pc-windows-gnu'
+python3 src/bootstrap/configure.py --experimental-targets=Xtensa --enable-extended --tools=clippy,cargo,rustfmt --dist-compression-formats='xz' --host 'x86_64-pc-windows-gnu' --enable-lld
 
 python3 x.py dist --stage 2
