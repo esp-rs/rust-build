485ec5bd31e80d958a766f299b4f4402decd4c0b
playfulFence <konnor1980@yandex.ru>
2023-02-14 15:46:10 +0100

    fix: add path to removals

--- build-template-project.sh
+++ build-template-project.sh
@@ -3,7 +3,7 @@ set -ef
 curl -L "https://github.com/cargo-generate/cargo-generate/releases/latest/download/cargo-generate-$(git ls-remote --refs --sort="version:refname" --tags "https://github.com/cargo-generate/cargo-generate" | cut -d/ -f3- | tail -n1)-aarch64-unknown-linux-gnu.tar.gz" -o "${HOME}/.cargo/bin/cargo-generate.tar.gz"
 tar -xzvf "${HOME}/.cargo/bin/cargo-generate.tar.gz" -C ${HOME}/.cargo/bin
 chmod u+x ${HOME}/.cargo/bin/cargo-generate
-rm LICENSE-APACHE LICENSE-MIT README.md
+rm ${HOME}/.cargo/bin/LICENSE-APACHE ${HOME}/.cargo/bin/LICENSE-MIT ${HOME}/.cargo/bin/README.md
 
 export USER=esp
 source /home/esp/export-esp.sh
