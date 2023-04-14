9ebd37f1c46011a030955c6ea17fcee8c6ab0213
Juraj Michalek <juraj.michalek@espressif.com>
2023-04-14 14:40:25 +0200

    ci: prepare for 1.69.0.0

--- support/rust-build/rename-rust-release.sh
+++ support/rust-build/rename-rust-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for I in *1.68.0-dev*; do
-     NEW_NAME=`echo $I | sed -e 's/1.68.0-dev/1.68.0.0/g'`
+for I in *1.69.0-dev*; do
+     NEW_NAME=`echo $I | sed -e 's/1.69.0-dev/1.69.0.0/g'`
      mv $I $NEW_NAME
 done
 
