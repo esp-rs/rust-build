7677a668403968f99b6d114162ea45f10d17d09d
Juraj Michálek <juraj.michalek@espressif.com>
2023-06-02 08:02:27 +0200

    ci: fix pwsh variable expansion

--- support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
+++ support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
@@ -2,6 +2,6 @@ winget install --id Git.Git --accept-source-agreements
 winget install --id MSYS2.MSYS2
 winget install --id 7zip.7zip
 
-setx /M PATH "%PATH%;C:\Program Files\7-Zip"
+setx /M PATH "$env:PATH;C:\Program Files\7-Zip"
 
 c:\msys64\msys2_shell.cmd -mingw64
