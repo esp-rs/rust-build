d9f4bbe121f21dbb7cb9720432e164bb380eea42
Juraj Michálek <juraj.michalek@espressif.com>
2023-06-05 16:21:56 +0200

    Build/1.70.0.1 (#211)
    * ci: bump up version number to 1.70.0.1

--- support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
+++ support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
@@ -1,8 +1,7 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-#choco install msys2 -y
-
 winget install --id Git.Git --accept-source-agreements
 winget install --id MSYS2.MSYS2
 winget install --id 7zip.7zip
+
+setx /M PATH "$env:PATH;C:\Program Files\7-Zip"
 
 c:\msys64\msys2_shell.cmd -mingw64
