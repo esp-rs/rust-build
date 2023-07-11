a6805fb6d1d34f7ef6fc96ad236d785a7d2a55a1
Juraj Michálek <juraj.michalek@espressif.com>
2023-07-11 14:31:29 +0200

    ci: add git to Windows path

--- support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
+++ support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
@@ -2,6 +2,6 @@ winget install --id Git.Git --accept-source-agreements
 winget install --id MSYS2.MSYS2
 winget install --id 7zip.7zip
 
-setx /M PATH "$env:PATH;C:\Program Files\7-Zip"
+setx /M PATH "$env:PATH;C:\Program Files\7-Zip;C:\Program Files\Git\bin"
 
 c:\msys64\msys2_shell.cmd -mingw64
