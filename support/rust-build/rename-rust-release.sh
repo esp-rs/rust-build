29e5564990df5932472156af4c126774a533852f
Juraj Michalek <juraj.michalek@espressif.com>
2023-08-23 13:39:08 +0200

    ci: bump up version to 1.72.0.0

--- support/rust-build/rename-rust-release.sh
+++ support/rust-build/rename-rust-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for I in *1.71.0-dev*; do
-     NEW_NAME=`echo $I | sed -e 's/1.71.0-dev/1.71.0.1/g'`
+for I in *1.72.0-dev*; do
+     NEW_NAME=`echo $I | sed -e 's/1.72.0-dev/1.72.0.0/g'`
      mv $I $NEW_NAME
 done
 
