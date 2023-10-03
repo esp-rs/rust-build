94a4b952fc830c138a44b5bb54c7cacfe30e4865
Juraj Michalek <juraj.michalek@espressif.com>
2023-10-03 13:44:07 +0200

    bump up version to 1.73.0.0

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -4,7 +4,7 @@ set -eu
 #set -v
 
 # Default values
-TOOLCHAIN_VERSION="1.72.1.0"
+TOOLCHAIN_VERSION="1.73.0.0"
 RUSTUP_HOME="${RUSTUP_HOME:-${HOME}/.rustup}"
 CARGO_HOME="${CARGO_HOME:-${HOME}/.cargo}"
 TOOLCHAIN_DESTINATION_DIR="${RUSTUP_HOME}/toolchains/esp"
