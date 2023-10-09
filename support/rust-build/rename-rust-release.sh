788131a1cbc42b3dbab4dc732f2390675c231558
Juraj Michálek <juraj.michalek@espressif.com>
2023-10-09 10:27:28 +0200

    Update version to 1.73.0.0 (#235)
    * bump up version to 1.73.0.0
    * ci: Windows - add Python to installed dependencies

--- support/rust-build/rename-rust-release.sh
+++ support/rust-build/rename-rust-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for I in *1.72.0-dev*; do
-     NEW_NAME=`echo $I | sed -e 's/1.72.0-dev/1.72.1.0/g'`
+for I in *1.73.0-dev*; do
+     NEW_NAME=`echo $I | sed -e 's/1.73.0-dev/1.73.0.0/g'`
      mv $I $NEW_NAME
 done
 
--- support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
+++ support/rust-build/x86_64-pc-windows-gnu/Prepare-BuildEnvironment.ps1
@@ -1,7 +1,10 @@
+# Requires elevation of privileges
+
 winget install --id Git.Git --accept-source-agreements
 winget install --id MSYS2.MSYS2
 winget install --id 7zip.7zip
+winget install --id Python.Python.3.12 --scope machine
 
-setx /M PATH "$env:PATH;C:\Program Files\7-Zip;C:\Program Files\Git\bin"
+setx /M PATH "$env:PATH;C:\Program Files\7-Zip"
 
 c:\msys64\msys2_shell.cmd -mingw64
