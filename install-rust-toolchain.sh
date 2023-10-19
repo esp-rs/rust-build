696ed8a7e82295ac2878a59a1c4af562b933388b
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2023-10-19 12:00:21 +0200

    Cleanup and CI update (#237)
    * ci: Refactor build-all workflow
    * chore: Remove unused file
    * docs: Add deprecation warning
    * ci: Rework releasing CI
    * ci: Allow only triggering some builds
    * ci: Update last release version

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -27,6 +27,7 @@ IS_SCCACHE_INSTALLED=0
 EXPORT_FILE="export-esp.sh"
 
 echo "WARNING: This installation script is deprecated. Use espup(https://github.com/esp-rs/espup) instead."
+echo "WARNING: This will be removed for Xtensa Rust 1.75.0."
 display_help() {
     echo "Usage: install-rust-toolchain.sh <arguments>"
     echo "Arguments: "
