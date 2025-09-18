5c5daa07a7bc683a39ad04e5543790071b6475f0
Scott Mabin <scott@mabez.dev>
2025-09-18 15:39:38 +0100

    fix windows gnu build

--- support/rust-build/x86_64-pc-windows-gnu/build-rust-toolchain-msys2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd c:
-git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git r
-cd r
-
-python3 src/bootstrap/configure.py --experimental-targets=Xtensa --enable-extended --tools=clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --dist-compression-formats='xz' --host 'x86_64-pc-windows-gnu' --enable-lld
-
-python3 x.py dist --stage 2
