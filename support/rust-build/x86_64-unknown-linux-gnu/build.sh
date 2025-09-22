bc365d40a4ccbded590ba4c7c95f2267da61af4f
Scott Mabin <scott@mabez.dev>
2025-09-22 10:40:10 +0100

    remove assumptions from docker builds

--- support/rust-build/x86_64-unknown-linux-gnu/build.sh
+++ support/rust-build/x86_64-unknown-linux-gnu/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git
+git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git -b "${RELEASE_BRANCH}"
 cd rust
-python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --release-description="${RELEASE_DESCRIPTION}" --enable-extended --enable-cargo-native-static --tools=clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --dist-compression-formats='xz' --enable-lld
+python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --release-description="${RELEASE_BRANCH}" --enable-extended --enable-cargo-native-static --tools=clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --dist-compression-formats='xz' --enable-lld
 python3 x.py dist --stage 2
