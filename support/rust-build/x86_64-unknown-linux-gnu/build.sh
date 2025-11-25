21882a8962f12eb2a3148f6ba293cfb282b3d7f6
Scott Mabin <scott@mabez.dev>
2025-11-25 15:05:42 +0000

    build then dist

--- support/rust-build/x86_64-unknown-linux-gnu/build.sh
+++ support/rust-build/x86_64-unknown-linux-gnu/build.sh
@@ -4,7 +4,6 @@ set -e
 
 git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git -b "esp-${RELEASE_VERSION}"
 cd rust
-python3 src/bootstrap/configure.py configure --experimental-targets=Xtensa --release-channel=nightly --enable-extended --tools=rustdoc,clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --enable-lld --prefix $(pwd)/build/esp --sysconfdir $(pwd)/build/esp-etc --disable-docs
+python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --enable-extended --tools=rustdoc,clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --enable-lld --prefix $(pwd)/build/esp --sysconfdir $(pwd)/build/esp-etc --host x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu --build aarch64-unknown-linux-gnu --disable-docs
 
-python3 x.py build
 python3 x.py dist
