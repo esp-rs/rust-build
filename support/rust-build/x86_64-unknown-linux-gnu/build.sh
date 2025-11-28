6d72730b118ad7be738df3e02e032bc39250854d
Scott Mabin <scott@mabez.dev>
2025-11-28 18:20:05 +0000

    just use --host, let bootstrap infer the rest. Add aarch64 first to ensure docs use the right host toolchain

--- support/rust-build/x86_64-unknown-linux-gnu/build.sh
+++ support/rust-build/x86_64-unknown-linux-gnu/build.sh
@@ -4,6 +4,5 @@ set -e
 
 git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git -b "esp-${RELEASE_VERSION}"
 cd rust
-python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --enable-extended --tools=rustdoc,clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --enable-lld --prefix $(pwd)/build/esp --sysconfdir $(pwd)/build/esp-etc --host x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu --build aarch64-unknown-linux-gnu --disable-docs
-
+python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --release-description="${RELEASE_VERSION}" --enable-extended --enable-cargo-native-static --tools=clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src,rustdoc --dist-compression-formats='xz' --enable-lld --host aarch64-unknown-linux-gnu,x86_64-unknown-linux-gnu --disable-docs
 python3 x.py dist
