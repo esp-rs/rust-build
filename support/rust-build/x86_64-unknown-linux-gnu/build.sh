776a0959523e9e74e448b2e7f8ccb67b355dfe07
Scott Mabin <scott@mabez.dev>
2025-12-03 10:15:35 +0000

    ensure rustdoc tool is enabled

--- support/rust-build/x86_64-unknown-linux-gnu/build.sh
+++ support/rust-build/x86_64-unknown-linux-gnu/build.sh
@@ -4,5 +4,5 @@ set -e
 
 git clone --recursive --depth 1 --shallow-submodules https://github.com/esp-rs/rust.git -b "esp-${RELEASE_VERSION}"
 cd rust
-python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --release-description="${RELEASE_VERSION}" --enable-extended --enable-cargo-native-static --tools=clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src,rustdoc --dist-compression-formats='xz' --enable-lld --host aarch64-unknown-linux-gnu,x86_64-unknown-linux-gnu --disable-docs
+python3 src/bootstrap/configure.py --experimental-targets=Xtensa --release-channel=nightly --release-description="${RELEASE_VERSION}" --enable-extended --enable-cargo-native-static --tools=rustdoc,clippy,cargo,rustfmt,rust-analyzer-proc-macro-srv,src --dist-compression-formats='xz' --enable-lld --host x86_64-unknown-linux-gnu
 python3 x.py dist
